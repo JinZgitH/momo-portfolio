ca1a8168e10b96ced5f5881655d5de073832b7ae
JinZgitH <water1942@gmail.com>
2025-05-15 13:25:14 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# momo-portfolio
+# Momo
+
+基于这个template： https://github.com/devsyedmohsin/portfolio-template
---
+++ script.js
@@ -0,0 +1,93 @@
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-items");
+const btnToggleNav = document.querySelector(".menu-btn");
+const workEls = document.querySelectorAll(".work-box");
+const workImgs = document.querySelectorAll(".work-img");
+const mainEl = document.querySelector("main");
+const yearEl = document.querySelector(".footer-text span");
+
+const toggleNav = () => {
+  nav.classList.toggle("hidden");
+  
+  // 移除transform，让菜单显示
+  if (!nav.classList.contains("hidden")) {
+    nav.style.transform = "translateX(0)";
+    btnToggleNav.style.transform = "translateX(260px)";
+    btnToggleNav.innerHTML = "× 关闭菜单";
+  } else {
+    nav.style.transform = "translateX(-100%)";
+    btnToggleNav.style.transform = "translateX(0)";
+    btnToggleNav.innerHTML = "☰ 菜单";
+  }
+
+  // Prevent screen from scrolling when menu is opened
+  document.body.classList.toggle("lock-screen");
+};
+
+btnToggleNav.addEventListener("click", toggleNav);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.localName === "a") {
+    toggleNav();
+  }
+});
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+    toggleNav();
+  }
+});
+
+// Animating work instances on scroll
+workImgs.forEach((workImg) => workImg.classList.add("transform"));
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    const [textbox, picture] = Array.from(entry.target.children);
+    if (entry.isIntersecting) {
+      picture.classList.remove("transform");
+      Array.from(textbox.children).forEach(
+        (el) => (el.style.animationPlayState = "running")
+      );
+    }
+  },
+  { threshold: 0.3 }
+);
+
+workEls.forEach((workEl) => {
+  observer.observe(workEl);
+});
+
+// Toggle theme and store user preferred theme for future
+const switchThemeEl = document.querySelector('input[type="checkbox"]');
+const storedTheme = localStorage.getItem("theme");
+
+switchThemeEl.checked = storedTheme === "dark" || storedTheme === null;
+
+switchThemeEl.addEventListener("click", () => {
+  const isChecked = switchThemeEl.checked;
+
+  if (!isChecked) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
+    switchThemeEl.checked = false;
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+// Trap the tab when menu is opened
+const lastFocusedEl = document.querySelector('a[data-focused="last-focused"]');
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Tab" && document.activeElement === lastFocusedEl) {
+    e.preventDefault();
+    btnToggleNav.focus();
+  }
+});
+
+yearEl.textContent = new Date().getFullYear();
